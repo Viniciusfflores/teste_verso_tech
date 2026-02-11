979ff0b36db29642b8d3cccfbc4d44a993dfc094
Vinicius Flores <viniciusflores.dev@gmail.com>
2026-02-10 22:39:06 -0300

    chore: bootstrap flutter project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const PokedexApp());
+}
+
+class PokedexApp extends StatelessWidget {
+  const PokedexApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(child: Text('Pokedex App')),
+      ),
+    );
+  }
+}
